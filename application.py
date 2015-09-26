789666c752eb12f92acb5086e5820f33552c1f82
Ryan McDevitt <mcdevitt.ryan@gmail.com>
2015-09-26 16:53:55 -0400

    deploy fix
    Fixes #
    
    Time: []

--- application.py
+++ application.py
@@ -12,14 +12,14 @@ if not DEBUG:
 
 db = SQLAlchemy(app)
 
-mail = Mail(app)
-
-import logging
-from logging.handlers import RotatingFileHandler
-file_handler = RotatingFileHandler(
-    'logs/fitbitAnalytics.log', 'a', 1 * 1024 * 1024, 10)
-file_handler.setFormatter(
-    logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-app.logger.setLevel(logging.INFO)
-file_handler.setLevel(logging.INFO)
-app.logger.addHandler(file_handler)
+# mail = Mail(app)
+#
+# import logging
+# from logging.handlers import RotatingFileHandler
+# file_handler = RotatingFileHandler(
+#     'logs/fitbitAnalytics.log', 'a', 1 * 1024 * 1024, 10)
+# file_handler.setFormatter(
+#     logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+# app.logger.setLevel(logging.INFO)
+# file_handler.setLevel(logging.INFO)
+# app.logger.addHandler(file_handler)
